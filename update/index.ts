53052bf03617f8fa2135141c63899bdc491902d8
Evan Bacon <bacon@expo.io>
2022-02-07 22:58:19 -0700

    remove extras

--- update/index.ts
+++ update/index.ts
@@ -1,34 +1,29 @@
 #!/usr/bin/env ts-node
-import assert from "assert";
+// import assert from "assert";
 import fs from "fs-extra";
 import path from "path";
 
 import { getLatestSpecAsync } from "./getLatestSpecAsync";
-import {
-  fileNameForSpec,
-  openPullRequestIfSpecDoesNotExistAsync,
-} from "./github";
 
-const isCI = process.env.CI === "true";
+export function fileNameForSpec(spec: any) {
+  return `specs/${spec.info.version ?? "unversioned"}.json`;
+}
 
 async function run() {
   const spec = await getLatestSpecAsync();
 
   const apiVersion = spec.info.version;
 
+  process.env.APPLE_API_VERSION = apiVersion;
   console.log("API:", apiVersion);
 
-  if (isCI) {
-    await openPullRequestIfSpecDoesNotExistAsync(spec);
-  } else {
-    const rootFolder = path.join(__dirname, "../");
-    const contents = JSON.stringify(spec, null, 2);
+  const rootFolder = path.join(__dirname, "../");
+  const contents = JSON.stringify(spec, null, 2);
 
-    await Promise.all([
-      fs.writeFile(path.join(rootFolder, fileNameForSpec(spec)), contents),
-      fs.writeFile(path.join(rootFolder, "specs/latest.json"), contents),
-    ]);
-  }
+  await Promise.all([
+    fs.writeFile(path.join(rootFolder, fileNameForSpec(spec)), contents),
+    fs.writeFile(path.join(rootFolder, "specs/latest.json"), contents),
+  ]);
 }
 
 // open pull request with new file
